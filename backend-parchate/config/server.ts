7025492096ec1a9ac8eaa221c3764b9051b335df
Alexander <alexandervalencia0121@gmail.com>
2025-03-01 23:26:16 -0500

    revert changes

--- backend-parchate/config/server.ts
+++ backend-parchate/config/server.ts
@@ -2,9 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://parchate-back.onrender.com'),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '240e355045561f4352111e7667cec448'),
-    },
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
