72a65861484b70534b0eae722d6cc1610b0a6c24
Alexander <alexandervalencia0121@gmail.com>
2025-03-01 23:21:32 -0500

    configure deploy

--- backend-parchate/config/server.ts
+++ backend-parchate/config/server.ts
@@ -2,7 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://parchate-back.onrender.com'),
-  app: {
-    keys: env.array('APP_KEYS'),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '240e355045561f4352111e7667cec448'),
+    },
   },
 });
