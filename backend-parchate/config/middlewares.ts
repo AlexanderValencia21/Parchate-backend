cc657c4efc628b0f299296aef02a5a628d003cd2
Alexander <alexandervalencia0121@gmail.com>
2025-03-01 21:37:38 -0500

    configure deploy environment

--- backend-parchate/config/middlewares.ts
+++ backend-parchate/config/middlewares.ts
@@ -1,9 +1,31 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', '*'],
+          'media-src': ["'self'", 'data:', 'blob:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+      frameguard: false,
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // En producción, cambia esto a tu dominio de Netlify
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['*'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
