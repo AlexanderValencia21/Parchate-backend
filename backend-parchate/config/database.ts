e1b2bd2be990a6af890b248702cc49a62ade44b3
Alexander <alexandervalencia0121@gmail.com>
2025-03-02 10:28:41 -0500

    adding support for postgres

--- backend-parchate/config/database.ts
+++ backend-parchate/config/database.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     mysql: {
@@ -25,12 +25,12 @@ export default ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'dpg-cv26t90gph6c73bdcasg-a'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+        database: env('DATABASE_NAME', 'strapi_bd'),
+        user: env('DATABASE_USERNAME', 'strapi_bd_user'),
+        password: env('DATABASE_PASSWORD', 'Wdh2Vx3hdsXAAdjtNBulKztDIABFrvjQ'),
+        ssl: env.bool('DATABASE_SSL', true) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
           ca: env('DATABASE_SSL_CA', undefined),
